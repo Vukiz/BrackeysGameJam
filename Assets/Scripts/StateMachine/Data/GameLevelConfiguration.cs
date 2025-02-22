7b2b29a94561ee2d2eb24abbf8f0919500d0e905
alekseeveg <vukiz1236@gmail.com>
2025-02-22 17:05:15 +0400

    fix for playerprefs

--- Assets/Scripts/StateMachine/Data/GameLevelConfiguration.cs
+++ Assets/Scripts/StateMachine/Data/GameLevelConfiguration.cs
@@ -8,27 +8,35 @@ namespace StateMachine.Data
         private const string TutorialFinishedKey = "IsTutorialFinished";
         private const string CurrentLevelIndexKey = "CurrentLevelIndex";
 
-        [SerializeField] private bool _defaultTutorialFinished;
-        [SerializeField] private int _defaultCurrentLevelIndex;
+        [SerializeField] private bool _isTutorialFinished;
+        [SerializeField] private int _currentLevelIndex;
 
         public bool IsTutorialFinished
         {
-            get => PlayerPrefs.GetInt(TutorialFinishedKey, _defaultTutorialFinished ? 1 : 0) == 1;
-            set => PlayerPrefs.SetInt(TutorialFinishedKey, value ? 1 : 0);
+            get => PlayerPrefs.GetInt(TutorialFinishedKey, _isTutorialFinished ? 1 : 0) == 1;
+            set
+            {
+                PlayerPrefs.SetInt(TutorialFinishedKey, value ? 1 : 0);
+                _isTutorialFinished = IsTutorialFinished;
+            }
         }
 
         public int CurrentLevelIndex
         {
-            get => PlayerPrefs.GetInt(CurrentLevelIndexKey, _defaultCurrentLevelIndex);
-            set => PlayerPrefs.SetInt(CurrentLevelIndexKey, value);
+            get => PlayerPrefs.GetInt(CurrentLevelIndexKey, _currentLevelIndex);
+            set
+            {
+                PlayerPrefs.SetInt(CurrentLevelIndexKey, value);
+                _currentLevelIndex = CurrentLevelIndex;
+            }
         }
 
-#if UNITY_EDITOR
+    #if UNITY_EDITOR
         private void OnValidate()
         {
             // This ensures changes in editor are immediately reflected in PlayerPrefs
-            IsTutorialFinished = _defaultTutorialFinished;
-            CurrentLevelIndex = _defaultCurrentLevelIndex;
+            IsTutorialFinished = _isTutorialFinished;
+            CurrentLevelIndex = _currentLevelIndex;
         }
 #endif
     }
